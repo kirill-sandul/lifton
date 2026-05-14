06079c3b2cfcade58500f5434d62095b4336b67d
Kirill Sandul <duboloxgog@gmail.com>
2026-05-14 14:37:55 +0200

    styles: add basic styling, variables, font and layout for auth page

---
+++ frontend/src/app/layouts/auth-layout/auth-layout.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-layout',
+  imports: [],
+  templateUrl: './auth-layout.html',
+  styleUrl: './auth-layout.scss',
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class AuthLayout {}
