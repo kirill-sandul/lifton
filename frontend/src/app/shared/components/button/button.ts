986ed76b5f87f0888d78b5bed3adbbec3be00eda
Kirill Sandul <duboloxgog@gmail.com>
2026-05-15 15:16:51 +0200

    feat(ui): added basic dark theme styles for future toggle

--- frontend/src/app/shared/components/button/button.ts
+++ frontend/src/app/shared/components/button/button.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { LucidePlay } from '@lucide/angular';
 
 type IconName = 'play' | 'msg' | ''
