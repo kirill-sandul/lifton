5312abf58c6262a14268435494bdea9af999b4f2
Kirill Sandul <duboloxgog@gmail.com>
2026-05-16 11:27:45 +0200

    refactor(ui): add file input component and global input styles

---
+++ frontend/src/app/shared/components/file-input/file-input.ts
@@ -0,0 +1,28 @@
+import { Component, input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { LucideUpload } from '@lucide/angular';
+
+@Component({
+  selector: 'app-file-input',
+  imports: [ReactiveFormsModule, LucideUpload],
+  templateUrl: './file-input.html',
+  styleUrl: './file-input.scss'
+})
+export class FileInputComponent {
+  id = input('')
+  placeholder = input('')
+  control = input<FormControl<File | null>>(new FormControl());
+  style = input<'outlined' | 'filled'>('outlined')
+
+  onFileUpload(event: Event){
+    const input = event.target as HTMLInputElement;
+
+    if(!input.files?.length) return;
+
+    const file = input.files[0];
+
+    this.control().setValue(file);
+
+    input.value = '';
+  }
+}
