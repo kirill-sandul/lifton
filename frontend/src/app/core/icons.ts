7fb6447a48d538193149e064959034ce42325df1
Kirill Sandul <duboloxgog@gmail.com>
2026-05-16 11:24:39 +0200

    chore(ui): add global icons import list

---
+++ frontend/src/app/core/icons.ts
@@ -0,0 +1,14 @@
+import { LucideAtSign, LucideEye, LucideEyeClosed, LucideFlaskConical, LucideMail, LucidePencil, LucidePhone, LucideRuler, LucideUserRound, LucideWeight } from "@lucide/angular";
+
+export const APP_ICONS = [
+  LucideEye,
+  LucideEyeClosed,
+  LucideUserRound,
+  LucideWeight,
+  LucideRuler,
+  LucidePencil,
+  LucideFlaskConical,
+  LucidePhone,
+  LucideMail,
+  LucideAtSign
+];
