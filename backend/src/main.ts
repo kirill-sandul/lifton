56f6beb207a77b0b78060cc4668eb2a9e884d61e
Kirill Sandul <duboloxgog@gmail.com>
2026-05-19 17:42:52 +0200

    feat(auth): add global prefix, enable CORS, validationPipe and prisma exception filter for error response

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import 'dotenv/config';
 import { AppModule } from './app.module';
+import { PrismaClientExceptionFilter } from './core/errors/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true
+  }))
+
+  app.useGlobalFilters(
+    new PrismaClientExceptionFilter()
+  );
+  
   app.use(cookieParser());
+  
+  app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true
+  })
   
   await app.listen(process.env.PORT ?? 3000);
 }
