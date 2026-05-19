af06e79eff018817b76fcc790bb1e9e3e5d2aa13
Kirill Sandul <duboloxgog@gmail.com>
2026-05-19 17:55:28 +0200

    fix(auth): add libphonenumber to equal phone validation on frontend and api

--- frontend/src/app/shared/validators/phone.validator.ts
+++ frontend/src/app/shared/validators/phone.validator.ts
@@ -1,11 +1,11 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+import { isValidPhoneNumber } from "libphonenumber-js";
 
 export function phoneValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     if(!control.value) return null;
 
-    const phone = control.value.replace(/\s/g, '');
-    const valid = /^\+?[1-9]\d{7,14}$/.test(phone);
+    const valid = isValidPhoneNumber(control.value);
 
     return valid ? null : { phone: true }
   }
