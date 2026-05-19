69d6e0ff8cf5dac5e1cb60857220cd2da14a87a4
Kirill Sandul <duboloxgog@gmail.com>
2026-05-19 17:31:59 +0200

    chore(db): changed pfpUrl in schema and in register dto

--- backend/src/features/auth/dto/register.dto.ts
+++ backend/src/features/auth/dto/register.dto.ts
@@ -1,4 +1,5 @@
 import { IsEmail, IsEnum, IsNumber, IsOptional, IsPhoneNumber, IsString, MaxLength, MinLength, ValidateIf } from "class-validator";
+import { Transform, Type } from "class-transformer";
 import { Goal, Role } from "src/generated/prisma/enums";
 
 export class RegisterDto {
@@ -9,6 +10,7 @@ export class RegisterDto {
   phone!: string;
 
   @IsNumber()
+  @Type(() => Number)
   age!: number;
 
   @IsString()
@@ -16,27 +18,32 @@ export class RegisterDto {
 
   @IsOptional()
   @IsString()
-  pfp?: string;
+  pfpUrl?: string;
+
+  @IsEnum(Role)
+  role!: Role;
 
   @ValidateIf(o => o.role === Role.CLIENT)
   @IsNumber()
+  @Type(() => Number)
   height!: number;
   
   @ValidateIf(o => o.role === Role.CLIENT)
   @IsNumber()
+  @Type(() => Number)
   bodyWeight!: number;
 
   @ValidateIf(o => o.role === Role.TRAINER)
   @IsOptional()
   @IsNumber()
+  @Type(() => Number)
   experience!: number;
   
+  @Transform(({ value }) => value === '' ? undefined : value)
+  @IsOptional()
   @MinLength(10)
   @MaxLength(400)
-  description!: string;
-
-  @IsEnum(Role)
-  role!: Role;
+  description?: string;
 
   @IsEnum(Goal)
   goal!: Goal;
