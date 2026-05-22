8314974b3843576a3d99df167a726aa4d7ca4c80
Kirill Sandul <duboloxgog@gmail.com>
2026-05-22 19:50:25 +0200

    chore(dto): add isString decorator for loginDto password field

--- backend/src/features/auth/dto/login.dto.ts
+++ backend/src/features/auth/dto/login.dto.ts
@@ -1,8 +1,9 @@
-import { IsEmail } from "class-validator";
+import { IsEmail, IsString } from "class-validator";
 
 export class LoginDto {
   @IsEmail()
   email!: string;
 
+  @IsString()
   password!: string;
 }
