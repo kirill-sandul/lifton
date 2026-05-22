0f62b5c15f55f689d9aba0ec7d5b54abf1551525
Kirill Sandul <duboloxgog@gmail.com>
2026-05-22 20:00:21 +0200

    feat(auth): login logic for login page, server errors display and styles for the error

--- frontend/src/app/features/auth/pages/login-page/login-page.ts
+++ frontend/src/app/features/auth/pages/login-page/login-page.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ButtonComponent } from '@shared/components/button/button';
 import { BaseInputComponent } from '@shared/components/base-input/base-input';
 import { APP_ICONS } from '@core/icons';
+import { AuthService } from '@features/auth/services/auth.service';
+import { LoginFormControls } from '@features/auth/models/auth.models';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
@@ -11,7 +17,10 @@ import { APP_ICONS } from '@core/icons';
   styleUrl: './login-page.scss',
 })
 export class LoginPageComponent {
-  loginForm = new FormGroup({
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  loginForm: FormGroup<LoginFormControls> = new FormGroup({
     email: new FormControl('', [
       Validators.email,
       Validators.required
@@ -21,7 +30,41 @@ export class LoginPageComponent {
     ])
   })
 
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.loginForm.valueChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
+      this.loginForm.setErrors(null);
+      this.loginForm.controls.email.setErrors(null);
+      this.loginForm.controls.password.setErrors(null);
+    });
+  }
+
+  checkCredentialsError({ error }: HttpErrorResponse){
+    if((error.statusCode === 401 && error.message === 'INVALID_CREDENTIALS') || error.statusCode === 400) {
+      const emailControl = this.loginForm.get('email');
+      const passwordControl = this.loginForm.get('password');
+      
+      emailControl?.setErrors({ serverCredentialsError: true });
+      passwordControl?.setErrors({ serverCredentialsError: true });
+
+      emailControl?.markAsTouched();
+      passwordControl?.markAsTouched();
+
+      this.loginForm.setErrors({
+        serverCredentialsError: true
+      })
+    }
+  }
+
   onSubmit(){
-    console.log(this.loginForm.value)
+    const { email, password } = this.loginForm.value;
+
+    if(email && password) this.authService.login({ email, password }).subscribe({
+      next: (response) => {
+        this.router.navigate(['/'])
+      },
+      error: (error) => this.checkCredentialsError(error)
+    })
   }
 }
