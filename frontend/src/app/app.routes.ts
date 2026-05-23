b42fbcbab146aa0528903696cf06f841a09f6d2b
Kirill Sandul <duboloxgog@gmail.com>
2026-05-23 12:12:02 +0200

    fix(auth): auth ' ' path redirect to login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,34 @@ import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout';
 import { LoginPageComponent } from './features/auth/pages/login-page/login-page';
 import { RegisterPageComponent } from './features/auth/pages/register-page/register-page';
 import { RegisterQuizPage } from '@features/auth/pages/register-quiz-page/register-quiz-page';
+import { DashboardPageComponent } from '@features/dashboard/pages/dashboard-page/dashboard-page';
+import { ProfilePageComponent } from '@features/profile/pages/profile-page/profile-page';
+
+import { authGuard } from '@core/guards/auth-guard';
+import { guestGuard } from '@core/guards/guest-guard';
 
 export const routes: Routes = [
   {
     path: '',
-    component: AuthLayoutComponent,
+    component: DashboardPageComponent,
+    canActivate: [authGuard],
     children: [
+      {
+        path: 'profile',
+        component: ProfilePageComponent
+      }
+    ]
+  },
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    canActivate: [guestGuard],
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'login'
+      },
       {
         path: 'login',
         component: LoginPageComponent
@@ -21,7 +43,8 @@ export const routes: Routes = [
     ],
   },
   {
-    path: 'register/quiz',
-    component: RegisterQuizPage
+    path: 'auth/register/quiz',
+    component: RegisterQuizPage,
+    canActivate: [guestGuard],
   }
 ];
