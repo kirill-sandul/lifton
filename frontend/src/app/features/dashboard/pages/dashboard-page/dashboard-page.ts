79b1f0da3e12ab83ff10badd7cf4b1f2254272ed
Kirill Sandul <duboloxgog@gmail.com>
2026-05-23 14:02:55 +0200

    feat(dashboard): base routing and ui shell WIP

--- frontend/src/app/features/dashboard/pages/dashboard-page/dashboard-page.ts
+++ frontend/src/app/features/dashboard/pages/dashboard-page/dashboard-page.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { AppLayoutComponent } from '@layouts/app-layout/app-layout';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [AppLayoutComponent, RouterOutlet],
+  imports: [],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.scss',
 })
