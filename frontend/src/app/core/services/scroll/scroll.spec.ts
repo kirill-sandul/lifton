64139359d184f5c67a0c78ce112ff4bf8e61940c
Kirill Sandul <duboloxgog@gmail.com>
2026-05-27 19:27:32 +0200

    feat(layout): app layout enhanced style and scroll service for header

--- frontend/src/app/core/services/scroll/scroll.spec.ts
+++ frontend/src/app/core/services/scroll/scroll.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UserService } from './user.service';
+import { Scroll } from './scroll';
 
-describe('UserService', () => {
-  let service: UserService;
+describe('Scroll', () => {
+  let service: Scroll;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    service = TestBed.inject(Scroll);
   });
 
   it('should be created', () => {
--- frontend/src/app/layouts/app-layout/app-layout.ts
+++ frontend/src/app/layouts/app-layout/app-layout.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@shared/components/header/header';
+import { ViewportScroller } from '@angular/common';
+import { ScrollService } from '@core/services/scroll/scroll';
 
 @Component({
   selector: 'app-app-layout',
@@ -9,11 +11,20 @@ import { HeaderComponent } from '@shared/components/header/header';
   styleUrl: './app-layout.scss',
 })
 export class AppLayoutComponent {
+  private scrollService = inject(ScrollService)
+
   ngOnInit(){
     document.body.classList.add('default-bg');
   }
 
   ngOnDestroy(){
     document.body.classList.remove('default-bg');
+  }
+
+  onScroll(event: Event){
+    const scrollY = (event.target as HTMLElement).scrollTop;
+
+    if(scrollY > 20) this.scrollService.setScrolled();
+    else this.scrollService.removeScrolled();
   }
 }
