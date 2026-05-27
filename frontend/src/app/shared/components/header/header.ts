65643e6450fe663e3aad16caad34b3eb90e9db2f
Kirill Sandul <duboloxgog@gmail.com>
2026-05-27 19:30:00 +0200

    feat(ui): header adding class when scrolled WIP. Moved profile widget to shared ui

--- frontend/src/app/shared/components/header/header.ts
+++ frontend/src/app/shared/components/header/header.ts
@@ -1,20 +1,36 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, effect, HostBinding, inject, signal } from '@angular/core';
 import { RouterLink } from "@angular/router";
-import { UserService } from '@core/services/user.service';
+import { UserService } from '@core/services/user/user.service';
 import { TabsButtonComponent } from '../tabs-button/tabs-button';
 import { UserRolePipe } from '@core/pipes/user-role.pipe';
 import { LucideBell, LucideMail, LucideSun, LucideMoveRight } from "@lucide/angular";
 import { LowerCasePipe } from '@angular/common';
+import { ScrollService } from '@core/services/scroll/scroll';
+import { ProfileWidgetComponent } from '@shared/components/profile-widget/profile-widget';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, TabsButtonComponent, UserRolePipe, LowerCasePipe, LucideSun, LucideBell, LucideMail, LucideMoveRight],
+  imports: [
+    RouterLink,
+    TabsButtonComponent,
+    LucideSun,
+    LucideBell,
+    LucideMail,
+    ProfileWidgetComponent,
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class HeaderComponent {
   userService = inject(UserService);
+  scrollService = inject(ScrollService);
   profile = this.userService.userProfile();
 
-  selectedTab = signal('Overview')
+  selectedTab = signal('Overview');
+  isScrolled = computed(() => this.scrollService.isScrolled());
+
+  @HostBinding('class.scrolled')
+  get scrolledClass() {
+    return this.isScrolled();
+  }
 }
