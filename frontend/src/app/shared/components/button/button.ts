a323d9f1f1c0ee9861faa640524c8ddca793e6d9
Kirill Sandul <duboloxgog@gmail.com>
2026-05-27 19:31:09 +0200

    feat(ui): compact button type and new preset icon

--- frontend/src/app/shared/components/button/button.ts
+++ frontend/src/app/shared/components/button/button.ts
@@ -1,14 +1,20 @@
 import { Component, input } from '@angular/core';
-import { LucidePlay } from '@lucide/angular';
+import {
+  LucideMailPlus,
+  LucidePlay,
+  LucideSend,
+  LucideSendHorizonal,
+  LucideSendHorizontal,
+} from '@lucide/angular';
 
 type IconName = 'play' | 'msg' | '';
 
 @Component({
   selector: 'button[liftonButton], a[liftonButton]',
-  imports: [LucidePlay],
+  imports: [LucidePlay, LucideSendHorizontal],
   templateUrl: './button.html',
   styleUrl: './button.scss',
 })
 export class ButtonComponent {
-  icon = input<IconName>('')
+  icon = input<IconName>('');
 }
