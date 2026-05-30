e9bc186396baedb479a521d4c0b35f0dddf0d2a7
Kirill Sandul <duboloxgog@gmail.com>
2026-05-30 12:00:37 +0200

    chore(user): prettier code

--- backend/src/features/user/user.controller.ts
+++ backend/src/features/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Patch, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtGuard } from 'src/core/guards/jwt.guard';
 import { CurrentUser } from 'src/core/decorators/current-user.decorator';
@@ -8,24 +17,30 @@ import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService){}
-  
+  constructor(private readonly userService: UserService) {}
+
   @Get('getProfile')
   @UseGuards(JwtGuard)
-  getProfile(@CurrentUser() user: { sub: string, role: Role }){
+  getProfile(@CurrentUser() user: { sub: string; role: Role }) {
     return this.userService.getProfile(user.sub);
   }
 
   @Post('editPfp')
   @UseInterceptors(FileInterceptor('newImg'))
   @UseGuards(JwtGuard)
-  editPfp(@UploadedFile() newImg: Express.Multer.File, @CurrentUser() user: { sub: string }){
+  editPfp(
+    @UploadedFile() newImg: Express.Multer.File,
+    @CurrentUser() user: { sub: string },
+  ) {
     return this.userService.editPfp(user.sub, newImg);
   }
-  
+
   @Patch('editProfile')
   @UseGuards(JwtGuard)
-  editProfile(@CurrentUser() user: { sub: string, role: Role }, @Body() dto: EditUserDto){
+  editProfile(
+    @CurrentUser() user: { sub: string; role: Role },
+    @Body() dto: EditUserDto,
+  ) {
     return this.userService.editProfile(user.sub, user.role, dto);
   }
 }
--- backend/src/features/user/user.service.ts
+++ backend/src/features/user/user.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/core/modules/prisma/prisma.service';
 import { StorageService } from 'src/core/modules/storage/storage.service';
 import { EditUserDto } from './dto/user.dto';
@@ -6,68 +10,111 @@ import { Role } from 'src/generated/prisma/enums';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService, private storage: StorageService){}
+  constructor(
+    private prisma: PrismaService,
+    private storage: StorageService,
+  ) {}
 
-  async getProfile(userId: string){
-    return await this.prisma.user.findUnique({
+  async getProfile(userId: string) {
+    const found = await this.prisma.user.findUnique({
       where: { id: userId },
-      include: { clientProfile: true, trainerProfile: true }
-    })
+      include: { clientProfile: true, trainerProfile: true },
+    });
+
+    if (found && found.clientProfile) {
+      const trainerId = found.clientProfile.assignedTrainerProfileId;
+      if (!trainerId) return;
+
+      const trainerProfile = await this.prisma.trainerProfile.findUnique({
+        where: { id: trainerId },
+      });
+
+      const resolvedTrainer = await this.prisma.user.findUnique({
+        where: { id: trainerProfile?.userId },
+        include: { trainerProfile: true },
+      });
+
+      return {
+        ...found,
+        clientProfile: {
+          ...found.clientProfile,
+          assignedTrainer: resolvedTrainer,
+        },
+      };
+    }
+    if (found && found.trainerProfile) return found;
+    else throw new NotFoundException('Cannot get user profile');
   }
 
-  async editPfp(userId: string, newImg: Express.Multer.File){
-    if(!newImg) throw new BadRequestException('No image uploaded');
-    
+  async editPfp(userId: string, newImg: Express.Multer.File) {
+    if (!newImg) throw new BadRequestException('No image uploaded');
+
     const user = await this.prisma.user.findUnique({
-      where: { id: userId }
-    })
+      where: { id: userId },
+    });
 
     let oldImgUrl = user?.pfpUrl;
     const newImgUrl = await this.storage.uploadFile(newImg, 'avatars');
 
-    if(newImgUrl && oldImgUrl) {
+    if (newImgUrl && oldImgUrl) {
       oldImgUrl = oldImgUrl.split('avatars/')[1];
       await this.storage.deleteFile(oldImgUrl);
     }
 
-    return await this.prisma.user.update({
+    return this.prisma.user.update({
       where: { id: userId },
       data: { pfpUrl: newImgUrl },
-      include: { clientProfile: true, trainerProfile: true }
-    })
+      include: { clientProfile: true, trainerProfile: true },
+    });
   }
-  
-  async editProfile(userId: string, role: Role, editProfileDto: EditUserDto){
-    const { fullName, age, goal, phone, email, description, bodyWeight, height, experience } = editProfileDto;
 
-    const preparedFormat: {[key: string]: any} = {
+  async editProfile(userId: string, role: Role, editProfileDto: EditUserDto) {
+    const {
+      fullName,
+      age,
+      goal,
+      phone,
+      email,
+      description,
+      bodyWeight,
+      height,
+      experience,
+    } = editProfileDto;
+
+    const preparedFormat: { [key: string]: any } = {
       fullName,
       age,
       email,
       phone,
       description,
       goal,
-      clientProfile: role === 'CLIENT' ? {
-        update: {
-          bodyWeight,
-          height
-        }
-      } : undefined,
-      trainerProfile: role === 'TRAINER' ? {
-        update: {
-          experience
-        }
-      } : undefined
-    }
-    
-    Object.keys(preparedFormat).forEach(key => {
-      if(preparedFormat[key] === null) preparedFormat[key] = undefined;
-    })
+      clientProfile:
+        role === 'CLIENT'
+          ? {
+              update: {
+                bodyWeight,
+                height,
+              },
+            }
+          : undefined,
+      trainerProfile:
+        role === 'TRAINER'
+          ? {
+              update: {
+                experience,
+              },
+            }
+          : undefined,
+    };
 
-    return await this.prisma.user.update({
+    Object.keys(preparedFormat).forEach((key) => {
+      if (preparedFormat[key] === null) preparedFormat[key] = undefined;
+    });
+
+    return this.prisma.user.update({
       where: { id: userId },
       data: { ...preparedFormat },
-      include: { clientProfile: true, trainerProfile: true }
-    })
+      include: { clientProfile: true, trainerProfile: true },
+    });
   }
 }
