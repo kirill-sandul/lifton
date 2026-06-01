363291dddf01bc77c0461113b7fcfbc5b8875053
Kirill Sandul <duboloxgog@gmail.com>
2026-06-01 11:45:38 +0200

    feat(dashboard): separate dashboard service into trainer and client services

---
+++ frontend/src/app/core/services/roles/trainer/trainer.service.ts
@@ -0,0 +1,127 @@
+import { computed, inject, Injectable } from '@angular/core';
+import { UserService } from '@core/services/user/user.service';
+import { ClientWorkoutOnDay } from '@core/models/training.models';
+import { UserGoal, UserProfile, UserRole } from '@core/models/user.models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TrainerService {
+  userService = inject(UserService);
+
+  detailedProfile = {};
+  clientsWorkoutsOnDay: ClientWorkoutOnDay[] = [
+    {
+      id: 'fepwlpwf',
+      fullName: 'Alicia Martinez',
+      pfpUrl:
+        'https://rdphpqnmuyeljhoecrfm.supabase.co/storage/v1/object/public/lifton/avatars/cd70a1a6-463a-4bbc-ad35-3c6bb45ed328.jpg',
+      age: 25,
+      active: true,
+      email: 'lohevol@mailer.com',
+      goal: UserGoal.MUSCLES,
+      role: UserRole.CLIENT,
+      phone: '+34651754534',
+      password: '$2b$10$p./GaYvLtKO/dZ5MyqbTAOPOYNryHEP4HjtRXYKkIvSrHaphibENi',
+      plannedWorkout: {
+        id: 'w1',
+        name: 'Powerlifting',
+        day: 'SUNDAY',
+        trainingPlanId: 'plan_1',
+        exercises: [
+          {
+            id: 'ex_1',
+            name: 'Bench Press',
+            workoutId: 'w1',
+            unit: 'kg',
+            sets: [
+              { id: 's1', exerciseId: 'ex_1', reps: 8, targetValue: 60 },
+              { id: 's2', exerciseId: 'ex_1', reps: 8, targetValue: 62.5 },
+              { id: 's3', exerciseId: 'ex_2', reps: 10, targetValue: 80 },
+            ],
+          },
+          {
+            id: 'ex_2',
+            name: 'Squat',
+            workoutId: 'w2',
+            unit: 'kg',
+            sets: [
+              { id: 's1', exerciseId: 'ex_1', reps: 8, targetValue: 60 },
+              { id: 's2', exerciseId: 'ex_1', reps: 8, targetValue: 62.5 },
+              { id: 's3', exerciseId: 'ex_2', reps: 10, targetValue: 80 },
+            ],
+          },
+          {
+            id: 'ex_3',
+            name: 'Deadlift',
+            unit: 'kg',
+            workoutId: 'w3',
+            sets: [
+              { id: 's1', exerciseId: 'ex_1', reps: 8, targetValue: 60 },
+              { id: 's2', exerciseId: 'ex_1', reps: 8, targetValue: 62.5 },
+              { id: 's3', exerciseId: 'ex_2', reps: 10, targetValue: 80 },
+            ],
+          },
+        ],
+      },
+    },
+  ];
+  clients: UserProfile[] = [
+    {
+      id: 'fepwdlpwf',
+      fullName: 'Alicia Martinez',
+      pfpUrl:
+        'https://rdphpqnmuyeljhoecrfm.supabase.co/storage/v1/object/public/lifton/avatars/cd70a1a6-463a-4bbc-ad35-3c6bb45ed328.jpg',
+      age: 25,
+      active: true,
+      email: 'lohevol@mailer.com',
+      goal: UserGoal.MUSCLES,
+      role: UserRole.CLIENT,
+      phone: '+34651754534',
+      password: '$2b$10$p./GaYvLtKO/dZ5MyqbTAOPOYNryHEP4HjtRXYKkIvSrHaphibENi',
+    },
+    {
+      id: 'fepw2lpwf',
+      fullName: 'John Martinez',
+      pfpUrl:
+        'https://rdphpqnmuyeljhoecrfm.supabase.co/storage/v1/object/public/lifton/avatars/284aac1e-55bf-431e-9f58-b815e344d001.png',
+      age: 25,
+      active: true,
+      email: 'lohevol@mailer.com',
+      goal: UserGoal.MUSCLES,
+      role: UserRole.CLIENT,
+      phone: '+34651754534',
+      password: '$2b$10$p./GaYvLtKO/dZ5MyqbTAOPOYNryHEP4HjtRXYKkIvSrHaphibENi',
+    },
+    {
+      id: 'fepw1lpwf',
+      fullName: 'Alicia Martinez',
+      pfpUrl:
+        'https://rdphpqnmuyeljhoecrfm.supabase.co/storage/v1/object/public/lifton/avatars/cd70a1a6-463a-4bbc-ad35-3c6bb45ed328.jpg',
+      age: 25,
+      active: true,
+      email: 'lohevol@mailer.com',
+      goal: UserGoal.MUSCLES,
+      role: UserRole.CLIENT,
+      phone: '+34651754534',
+      password: '$2b$10$p./GaYvLtKO/dZ5MyqbTAOPOYNryHEP4HjtRXYKkIvSrHaphibENi',
+    },
+    {
+      id: 'fepw6lpwf',
+      fullName: 'Alicia Martinez',
+      pfpUrl:
+        'https://rdphpqnmuyeljhoecrfm.supabase.co/storage/v1/object/public/lifton/avatars/cd70a1a6-463a-4bbc-ad35-3c6bb45ed328.jpg',
+      age: 25,
+      active: true,
+      email: 'lohevol@mailer.com',
+      goal: UserGoal.MUSCLES,
+      role: UserRole.CLIENT,
+      phone: '+34651754534',
+      password: '$2b$10$p./GaYvLtKO/dZ5MyqbTAOPOYNryHEP4HjtRXYKkIvSrHaphibENi',
+    },
+  ];
+
+  noData = computed((): boolean => {
+    return !this.userService.userProfile()?.trainerProfile!.clients?.length;
+  });
+}
