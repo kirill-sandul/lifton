779e088591e22f4c599d8d75bb6f04f496d0d1a9
Kirill Sandul <duboloxgog@gmail.com>
2026-06-01 11:54:35 +0200

    feat(dashboard): add base clients list widget

---
+++ frontend/src/app/features/dashboard/components/trainer/clients-list-widget/clients-list-widget.ts
@@ -0,0 +1,16 @@
+import { Component, inject } from '@angular/core';
+import { ClientPreviewComponent } from '@shared/components/client-preview/client-preview';
+import { TrainerService } from '@core/services/roles/trainer/trainer.service';
+import { ButtonComponent } from '@shared/components/button/button';
+import { LucideArrowUpRight } from '@lucide/angular';
+
+@Component({
+  selector: 'app-clients-list-widget',
+  imports: [ClientPreviewComponent, ButtonComponent, LucideArrowUpRight],
+  templateUrl: './clients-list-widget.html',
+  styleUrl: './clients-list-widget.scss',
+})
+export class ClientsListWidgetComponent {
+  trainerService = inject(TrainerService);
+  clientsList = this.trainerService.clients.slice(0, 3);
+}
