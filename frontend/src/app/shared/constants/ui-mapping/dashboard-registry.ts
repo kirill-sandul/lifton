ecdfd0dc16584bddb1189a330061414ddc3eb4be
Kirill Sandul <duboloxgog@gmail.com>
2026-06-01 12:11:04 +0200

    feat(dashboard): add trainer widgets registry + clean imports

--- frontend/src/app/shared/constants/ui-mapping/dashboard-registry.ts
+++ frontend/src/app/shared/constants/ui-mapping/dashboard-registry.ts
@@ -1,17 +1,31 @@
-import { Type } from "@angular/core";
-import { UserRole } from "@core/models/user.models";
-import { TrainerInfoWidgetComponent } from "@features/dashboard/components/client/trainer-info-widget/trainer-info-widget";
-import { WorkoutWidgetComponent } from "@features/dashboard/components/client/workout-widget/workout-widget";
-import { CompletionWidgetComponent, } from '@features/dashboard/components/client/program-completion-widget/program-completion-widget';
-import { StreakWidgetComponent } from '@features/dashboard/components/client/streak-widget/streak-widget';
-import { TargetsWidgetComponent } from '@features/dashboard/components/client/targets-widget/targets-widget';
-import { ScheduleWidgetComponent } from '@features/dashboard/components/client/schedule-widget/schedule-widget';
-import { ProgressChartComponent } from '@features/dashboard/components/client/progress-chart/progress-chart';
+import { Type } from '@angular/core';
+import { UserRole } from '@core/models/user.models';
+
+import {
+  WorkoutWidgetComponent,
+  TrainerInfoWidgetComponent,
+  CompletionWidgetComponent,
+  StreakWidgetComponent,
+  TargetsWidgetComponent,
+  ClientScheduleWidgetComponent,
+  ProgressChartComponent,
+} from '@features/dashboard/components/client';
+
+import {
+  AvgProgressWidgetComponent,
+  AdherenceRateWidgetComponent,
+  CompletedWorkoutsWidgetComponent,
+  ActiveProgramsWidgetComponent,
+  TrainerScheduleWidgetComponent,
+  TodoWidgetComponent,
+  ClientsListWidgetComponent,
+  ClientsProgressChartComponent,
+} from '@features/dashboard/components/trainer';
 
 type WidgetList = {
-  component: Type<any>,
-  grid: string
-}
+  component: Type<any>;
+  grid: string;
+};
 
 export type DashboardWidgetRegistry = Record<UserRole, WidgetList[]>;
 
@@ -38,22 +52,55 @@ export const DASHBOARD_WIDGET_REGISTRY: DashboardWidgetRegistry = {
       grid: 'grid-rows-3-4 grid-cols-2-3',
     },
     {
-      component: ScheduleWidgetComponent,
-      grid: 'grid-rows-3-4 grid-cols-3-4'
+      component: ClientScheduleWidgetComponent,
+      grid: 'grid-rows-3-4 grid-cols-3-4',
     },
     {
       component: ProgressChartComponent,
       grid: 'grid-rows-1-3 grid-cols-2-3',
-    }
+    },
   ],
-  TRAINER: []
+  TRAINER: [
+    {
+      component: AvgProgressWidgetComponent,
+      grid: 'grid-rows-1-2',
+    },
+    {
+      component: AdherenceRateWidgetComponent,
+      grid: 'grid-rows-1-2',
+    },
+    {
+      component: CompletedWorkoutsWidgetComponent,
+      grid: 'grid-rows-1-2 min-size',
+    },
+    {
+      component: ActiveProgramsWidgetComponent,
+      grid: 'grid-rows-1-2 ',
+    },
+    {
+      component: TrainerScheduleWidgetComponent,
+      grid: 'grid-rows-1-2 grid-span-3 max-size',
+    },
+    {
+      component: ClientsListWidgetComponent,
+      grid: 'grid-rows-2-3',
+    },
+    {
+      component: ClientsProgressChartComponent,
+      grid: 'grid-rows-2-3 grid-span-3',
+    },
+    {
+      component: TodoWidgetComponent,
+      grid: 'grid-rows-2-3 grid-cols-5-8',
+    },
+  ],
 };
 
-export const NO_PROGRAM_WIDGET_REGISTRY: DashboardWidgetRegistry = {
+export const NO_DATA_WIDGET_REGISTRY: DashboardWidgetRegistry = {
   CLIENT: [
     {
       component: TrainerInfoWidgetComponent,
-      grid: 'grid-rows-1-3',
+      grid: 'grid-rows-1-3 grid-cols-1-2',
     },
     {
       component: WorkoutWidgetComponent,
@@ -64,8 +111,8 @@ export const NO_PROGRAM_WIDGET_REGISTRY: DashboardWidgetRegistry = {
       grid: 'grid-rows-2-3 grid-cols-2-3',
     },
     {
-      component: ScheduleWidgetComponent,
-      grid: 'grid-rows-3-4 grid-cols-1-1',
+      component: ClientScheduleWidgetComponent,
+      grid: 'grid-rows-2-3 grid-cols-1-2',
     },
     {
       component: ProgressChartComponent,
