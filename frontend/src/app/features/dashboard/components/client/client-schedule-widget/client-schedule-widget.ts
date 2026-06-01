6b20e557e5c19cb2cdf9651b92b49072d022b1fa
Kirill Sandul <duboloxgog@gmail.com>
2026-06-01 11:49:17 +0200

    feat(dashboard): rename client schedule widget to separate from trainer schedule

---
+++ frontend/src/app/features/dashboard/components/client/client-schedule-widget/client-schedule-widget.ts
@@ -0,0 +1,47 @@
+import { Component, effect, inject, signal } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { DashboardService } from '@features/dashboard/services/dashboard.service';
+import { Workout } from '@core/models/training.models';
+import { CalendarWidgetComponent } from '@features/dashboard/components/calendar-widget/calendar-widget';
+
+@Component({
+  selector: 'app-client-schedule-widget',
+  imports: [DatePipe, CalendarWidgetComponent],
+  templateUrl: './client-schedule-widget.html',
+  styleUrl: './client-schedule-widget.scss',
+})
+export class ClientScheduleWidgetComponent {
+  dashboardService = inject(DashboardService);
+
+  selectedDay = signal<Date>(new Date());
+  dayContent = signal<Workout | null>(null);
+  schedule = this.dashboardService.programSchedule;
+
+  constructor() {
+    effect(() => {
+      const workout = this.getDayExercises(this.selectedDay());
+
+      this.dayContent.set(workout ?? null);
+    });
+  }
+
+  getDayExercises(day: Date) {
+    this.selectedDay.set(day);
+
+    const dayIdx = day.getDay();
+
+    const weekDays: Record<string, string> = {
+      '0': 'SUNDAY',
+      '1': 'MONDAY',
+      '2': 'TUESDAY',
+      '3': 'WEDNESDAY',
+      '4': 'THURSDAY',
+      '5': 'FRIDAY',
+      '6': 'SATURDAY',
+    };
+
+    if (!this.schedule) return;
+
+    return this.schedule.find((d) => d.day === weekDays[dayIdx.toString()]);
+  }
+}
