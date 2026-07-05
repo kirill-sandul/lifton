8414e7ed67919e2d177d8473e627cdbd1f3b48f5
Kirill Sandul <duboloxgog@gmail.com>
2026-07-05 12:56:45 +0200

    feat: provide all icons in app config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,9 @@
-import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
@@ -7,7 +12,8 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { apiInterceptor } from '@core/interceptors/api.interceptor';
 import { AuthService } from '@features/auth/services/auth.service';
-
+import { provideLucideIcons } from '@lucide/angular';
+import { APP_ICONS } from '@core/icons';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,6 +24,7 @@ export const appConfig: ApplicationConfig = {
       const authService = inject(AuthService);
       return firstValueFrom(authService.refresh());
     }),
-    provideCharts(withDefaultRegisterables())
+    provideLucideIcons(...APP_ICONS),
+    provideCharts(withDefaultRegisterables()),
   ],
 };
