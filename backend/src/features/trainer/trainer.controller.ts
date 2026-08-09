534d88719e2b736aab32c4ff445081a718e2af91
Kirill Sandul <duboloxgog@gmail.com>
2026-08-09 20:25:41 +0200

    feat(create-program): schema updates; prisma updates; trainer controller with role guard

--- backend/src/features/trainer/trainer.controller.ts
+++ backend/src/features/trainer/trainer.controller.ts
@@ -3,8 +3,12 @@ import { JwtGuard } from '../../core/guards/jwt.guard';
 import { CurrentUser } from '../../core/decorators/current-user.decorator';
 import { TrainerService } from './trainer.service';
 import { AssignmentService } from '../../core/modules/assignment/assignment.service';
+import { RoleGuard } from '../../core/guards/role.guard';
+import { Roles } from '../../core/decorators/roles.decorator';
 
 @Controller('trainer')
+@Roles('TRAINER')
+@UseGuards(RoleGuard)
 export class TrainerController {
   constructor(
     private readonly trainerService: TrainerService,
